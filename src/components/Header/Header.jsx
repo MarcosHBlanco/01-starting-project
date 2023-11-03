04f765d4318aeb8e7548766cd37a9db9d5127416
MarcosHBlanco <marcos.blanco@al.infnet.edu.br>
2023-11-03 16:39:24 -0300

    Outputting Lista Data Dinamically

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import atomImg from '../../assets/react-core-concepts.png';
+import './Header.css';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+    const randomWordPlay = reactDescriptions[getRandomInt(2)];
+
+    return (
+        <header>
+            <img src={atomImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {randomWordPlay} React concepts you will need for almost any app you are
+                going to build!
+            </p>
+        </header>
+    );
+}
